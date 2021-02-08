e41eda56ca076fac004a3044a9418c539c287038
Luis Juarez <luisisthejuarez@gmail.com>
2021-02-07 21:20:02 -0500

    init

---
+++ app/src/main/java/edu/grcc/luisjuarez/spanishdictionary/DatabaseAccess.java
@@ -0,0 +1,85 @@
+package edu.grcc.luisjuarez.spanishdictionary;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+    private static DatabaseAccess instance;
+
+    private DatabaseAccess(Context context, String databaseName) {
+        this.openHelper = new DatabaseOpenHelper(context, databaseName);
+
+    }
+
+    public static DatabaseAccess getInstance(Context context, String databaseName) {
+        if (instance == null) {
+            instance = new DatabaseAccess(context, databaseName);
+        }
+        return instance;
+    }
+
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        if (database != null) {
+            this.database.close();
+        }
+    }
+
+    public List<String> getString(String tableName, String fieldName) {
+        List<String> list = new ArrayList<>();
+        String SQL = "SELECT " + tableName + " FROM " + fieldName;
+        Cursor cursor = database.rawQuery("SELECT " + fieldName + " FROM " + tableName, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            list.add(cursor.getString(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return list;
+    }
+
+    public List<Integer> getInteger(String tableName, String fieldName) {
+        List<Integer> list = new ArrayList<>();
+        String SQL = "SELECT " + tableName + " FROM " + fieldName;
+        Cursor cursor = database.rawQuery("SELECT " + fieldName + " FROM " + tableName, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            list.add(cursor.getInt(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return list;
+    }
+
+    public List<Double> getDouble(String tableName, String fieldName) {
+        List<Double> list = new ArrayList<>();
+        String SQL = "SELECT " + tableName + " FROM " + fieldName;
+        Cursor cursor = database.rawQuery("SELECT " + fieldName + " FROM " + tableName, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            list.add(cursor.getDouble(0));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return list;
+    }
+
+
+    class DatabaseOpenHelper extends SQLiteAssetHelper {
+        private static final int DATABASE_VERSION = 1;
+
+        public DatabaseOpenHelper(Context context, String databaseName) {
+            super(context, databaseName, null, DATABASE_VERSION);
+        }
+    }
+}
---
+++ app/src/main/java/edu/grcc/luisjuarez/spanishdictionary/MainActivity.java
@@ -0,0 +1,82 @@
+package edu.grcc.luisjuarez.spanishdictionary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> spanishWords;
+    List<String> englishWords;
+    List<String> spanishSorted;
+    ListView listViewSpanish;
+    ArrayAdapter arrayAdapter;
+    String databaseName = "Spanish.db";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get access to database data
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this, databaseName);
+        databaseAccess.open();
+
+        spanishWords = databaseAccess.getString("words", "spanish");
+        spanishSorted = databaseAccess.getString("words", "spanish");
+        englishWords = databaseAccess.getString("words", "english");
+        Collections.sort(spanishSorted);
+
+        listViewSpanish = findViewById(R.id.listViewSpanish);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, spanishSorted);
+        listViewSpanish.setAdapter(arrayAdapter);
+
+        listViewSpanish.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String spanishWord = listViewSpanish.getItemAtPosition(position).toString();
+                String englishWord = "";
+                for (int i = 0; i< spanishWords.size(); i++) {
+                    if (spanishWords.get(i).equals(spanishWord)) {
+                        englishWord = englishWords.get(i);
+                    }
+                }
+                Toast.makeText(getApplicationContext(), "The word " + spanishWord + " means " + englishWord, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
